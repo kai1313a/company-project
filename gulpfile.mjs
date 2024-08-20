71c3c25251be6182f068ac6335e779698ecd4dc4
saseunghan <han7182002@naver.com>
2024-08-20 12:55:22 +0900

    걸프 셋팅 완료

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -4,44 +4,43 @@ import sassModule from 'sass';
 import browserSync from 'browser-sync';
 import { deleteAsync as del } from 'del'; // del을 named import로 가져오기
 import fileinclude from 'gulp-file-include';
-import prettyHtml from 'gulp-pretty-html';
 
 const scss = sass(sassModule);
 const browserSyncInstance = browserSync.create();
 
 // 소스 파일 경로
 const PATH = {
-  HTML: "./src",
-  COMMON_HTML: "./src/html",
+  HTML: './src',
+  COMMON_HTML: './src/html',
   ASSETS: {
-    COMMON_CSS: "./src/assets/sass",
-    COMMON_IMG: "./src/assets/img",
-    COMMON_SCRIPT: "./src/assets/js",
-    FONT: "./src/assets/fonts",
+    COMMON_CSS: './src/assets/sass',
+    COMMON_IMG: './src/assets/img',
+    COMMON_SCRIPT: './src/assets/js',
+    FONT: './src/assets/fonts',
   },
 };
 
 // 작업물 경로
 const DEV_PATH = {
-  CLEAN: "./dev",
-  HTML: "./dev/src",
+  CLEAN: './dev',
+  HTML: './dev/src',
   ASSETS: {
-    COMMON_CSS: "./dev/src/assets/css",
-    COMMON_IMG: "./dev/src/assets/img",
-    COMMON_SCRIPT: "./dev/src/assets/js",
-    FONT: "./dev/src/assets/fonts",
+    COMMON_CSS: './dev/src/assets/css',
+    COMMON_IMG: './dev/src/assets/img',
+    COMMON_SCRIPT: './dev/src/assets/js',
+    FONT: './dev/src/assets/fonts',
   },
 };
 
 // 산출물 경로
 const DEST_PATH = {
-  CLEAN: "./dist",
-  HTML: "./dist/src",
+  CLEAN: './dist',
+  HTML: './dist/src',
   ASSETS: {
-    COMMON_CSS: "./dist/src/assets/css",
-    COMMON_IMG: "./dist/src/assets/img",
-    COMMON_SCRIPT: "./dist/src/assets/js",
-    FONT: "./dist/src/assets/fonts",
+    COMMON_CSS: './dist/src/assets/css',
+    COMMON_IMG: './dist/src/assets/img',
+    COMMON_SCRIPT: './dist/src/assets/js',
+    FONT: './dist/src/assets/fonts',
   },
 };
 
@@ -56,7 +55,8 @@ export const cleanDist = async () => {
 
 // SCSS compilation
 export const scssCompile = () => {
-  return gulp.src(PATH.ASSETS.COMMON_CSS + '/**/*.scss')
+  return gulp
+    .src(PATH.ASSETS.COMMON_CSS + '/**/*.scss')
     .pipe(scss({ outputStyle: 'expanded' }).on('error', scss.logError))
     .pipe(gulp.dest(DEV_PATH.ASSETS.COMMON_CSS))
     .pipe(browserSyncInstance.stream());
@@ -64,18 +64,22 @@ export const scssCompile = () => {
 
 // HTML tasks
 export const html = () => {
-  return gulp.src(PATH.HTML + '/**/*.html')
-    .pipe(fileinclude({
-      prefix: '@@',
-      basepath: '@file',
-    }))
+  return gulp
+    .src(PATH.HTML + '/**/*.html')
+    .pipe(
+      fileinclude({
+        prefix: '@@',
+        basepath: '@file',
+      })
+    )
     .pipe(gulp.dest(DEV_PATH.HTML))
     .pipe(browserSyncInstance.stream());
 };
 
 // Script tasks
 export const scriptConcat = () => {
-  return gulp.src(PATH.ASSETS.COMMON_SCRIPT + '/**/*.js')
+  return gulp
+    .src(PATH.ASSETS.COMMON_SCRIPT + '/**/*.js')
     .pipe(gulp.dest(DEV_PATH.ASSETS.COMMON_SCRIPT))
     .pipe(browserSyncInstance.stream());
 };
@@ -99,7 +103,7 @@ export const browserSyncTask = () => {
       DEV_PATH.HTML + '/**/*.html',
       DEV_PATH.ASSETS.COMMON_CSS + '/**/*.css',
       DEV_PATH.ASSETS.COMMON_SCRIPT + '/**/*.js',
-      DEV_PATH.ASSETS.COMMON_IMG + '/**/*.*'
+      DEV_PATH.ASSETS.COMMON_IMG + '/**/*.*',
     ],
     startPath: '/',
     ghostMode: false,
