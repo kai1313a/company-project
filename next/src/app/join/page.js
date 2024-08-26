234b8ff673fd4b712a84fa45228bf35c4f0e08ed
saseunghan <han7182002@naver.com>
2024-08-26 14:17:58 +0900

    sass 설치 및 라이브러리 및 이미지폴더 생성

--- next/src/app/join/page.js
+++ next/src/app/join/page.js
@@ -1,37 +1,52 @@
+'use client';
+
 import Link from 'next/link'
+import { useEffect, useState } from 'react';
+import parse from 'html-react-parser';
 
 export default function join() {
+
+    const userName = 'Yenny'
+    const [mealCategories, setMealCategories] = useState([]);
+
+    useEffect(() => {
+        setMealCategories([
+            { title: '커피, 음료,<br> 디저트', image: './image/join/meal-item01-greencoffee.png', alt: '커피' },
+            { title: '아침', image: './image/join/meal-item02-toster.png', alt: '토스트' },
+            { title: '점심', image: './image/join/meal-item03-rice.png', alt: '밥' },
+            { title: '저녁, 회식', image: './image/join/meal-item04-beer.png', alt: '맥주' },
+        ]);
+    }, []);
+
     return (
         <div className="flex flex-col items-center min-h-screen p-20 bg-white">
-            <div className='company'>
-                <h2 className='company__title mb-8 text-left text-black'>오늘도 존버 <span className="color">Yenny</span>님<br />건강한 <span className="bt-color">회사세끼</span> 되세요</h2>
+            <main className="meal">
+                <h2 className="meal__title text-black my-9 text-2xl">
+                    오늘도 존버 <span>{userName}</span>님<br />건강한 회사세끼 되세요.
+                </h2>
 
-                <ul className='company__list grid grid-cols-2 gap-x-8 gap-y-8'>
-                    <li className='company__item company__item--1 p-5 bg-zinc-600 rounded-xl'>
-                        <Link href="/">
-                            <h3 className='text-black'>커피, 음료, <br /> 디저트</h3>
-                        </Link>
-                    </li>
-                    <li className='company__item company__item--2 p-5 bg-zinc-600 rounded-xl'>
-                        <Link href="/">
-                            <h3 className='text-black'>아침</h3>
-                        </Link>
-                    </li>
-                    <li className='company__item company__item--3 p-5 bg-zinc-600 rounded-xl'>
-                        <Link href="/">
-                            <h3 className='text-black'>점심</h3>
-                        </Link>
-                    </li>
-                    <li className='company__item company__item--4 p-5 bg-zinc-600 rounded-xl'>
-                        <Link href="/">
-                            <h3 className='text-black'>저녁, 회식</h3>
-                        </Link>
-                    </li>
-                    <li className='company__item company__item--5 p-5s bg-zinc-600 rounded-xl'>
-                        광고배너
-                    </li>
+                <ul className="meal-list grid gap-8 grid-cols-2 my-9">
+                    {mealCategories.map((item, i) => (
+                        <li
+                            key={item.title}
+                            className={`meal-item relative rounded-lg bg-slate-300 p-5 pb-11 w-[166px] h-[115px] ${i === 2 ? 'meal-item--color' : ''}`}
+                        >
+                            <Link href="/">
+                                <h3 className="text-black text-lg">
+                                    {parse(item.title)}
+                                </h3>
+                                <div className="img-box w-20 h-20 absolute bottom-[-17px] right-0">
+                                    <img
+                                        src={item.image}
+                                        alt={item.alt}
+                                    />
+                                </div>
+                            </Link>
+                        </li>
+                    ))}
                 </ul>
-            </div>
-        </div >
+
+            </main>
+        </div>
     );
 }
--- next/src/app/layout.js
+++ next/src/app/layout.js
@@ -1,4 +1,5 @@
 import "./styles/globals.css";
+import "./styles/join.scss";
 
 export default function RootLayout({ children }) {
   return (
