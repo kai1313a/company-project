50136080c335ffefd436a710385af3ca5aeeb4ac
saseunghan <han7182002@naver.com>
2024-08-27 15:18:01 +0900

    mixin 생성 및 scss 구조 변경

--- next/src/app/join/page.js
+++ next/src/app/join/page.js
@@ -21,21 +21,21 @@ export default function join() {
     return (
         <div className="flex flex-col items-center min-h-screen p-20 bg-white">
             <main className="meal">
-                <h2 className="meal__title text-black my-9 text-2xl">
-                    오늘도 존버 <span>{userName}</span>님<br />건강한 회사세끼 되세요.
+                <h2 className="meal__title text-black my-9 font-semibold">
+                    오늘도 존버 <span className='userName'>{userName}</span>님<br />건강한 <span className='companyUser relative after:absolute after:bottom-0 after:left-0 after:w-full after:h-[10px] after:bg-[#4DF5C3] after:z-[-1]'>회사세끼</span> 되세요.
                 </h2>
 
                 <ul className="meal-list grid gap-8 grid-cols-2 my-9">
                     {mealCategories.map((item, i) => (
                         <li
                             key={item.title}
-                            className={`meal-item relative rounded-lg bg-slate-300 p-5 pb-11 w-[166px] h-[115px] ${i === 2 ? 'meal-item--color' : ''}`}
+                            className={`meal-item relative rounded-lg font-semibold relative after:absolute after:top-0 after:left-0 after:w-full after:h-full after:bg-gradient-to-tr after:from-[#e2e2e2] after:to-[#666666] after:opacity-20 after:rounded-[10px] ${i === 2 ? 'meal-item--color' : ''}`}
                         >
-                            <Link href="/">
+                            <Link href="/" className='relative w-[166px] h-[115px] p-5 pb-11 block z-10'>
                                 <h3 className="text-black text-lg">
                                     {parse(item.title)}
                                 </h3>
-                                <div className="img-box w-20 h-20 absolute bottom-[-17px] right-0">
+                                <div className="img-box md:w-auto h-20 absolute w-20 bottom-[-17px] right-0">
                                     <img
                                         src={item.image}
                                         alt={item.alt}
@@ -45,8 +45,8 @@ export default function join() {
                         </li>
                     ))}
                 </ul>
-
+                <textarea className='meal__banner w-full p-5 rounded-xl text-black font-semibold rerative text-lg opacity-20 bg-gradient-to-tr from-[#e2e2e2] to-[#666666] ' defaultValue="광고배너" ></textarea>
             </main>
-        </div>
+        </div >
     );
 }
