6c534f7c66c084ad48956cee026a5b9b74add7b7
saseunghan <han7182002@naver.com>
2024-08-27 17:56:25 +0900

    mixin 수정 및 css 삭제

--- src/app/join/page.js
+++ src/app/join/page.js
@@ -25,17 +25,17 @@ export default function join() {
                     오늘도 존버 <span className='userName'>{userName}</span>님<br />건강한 <span className='companyUser relative after:absolute after:bottom-0 after:left-0 after:w-full after:h-[10px] after:bg-[#4DF5C3] after:z-[-1]'>회사세끼</span> 되세요.
                 </h2>
 
-                <ul className="meal-list grid gap-8 grid-cols-2 my-9">
+                <ul className="meal-list grid grid-cols-2">
                     {mealCategories.map((item, i) => (
                         <li
                             key={item.title}
                             className={`meal-item relative rounded-lg font-semibold relative after:absolute after:top-0 after:left-0 after:w-full after:h-full after:bg-gradient-to-tr after:from-[#e2e2e2] after:to-[#666666] after:opacity-20 after:rounded-[10px] ${i === 2 ? 'meal-item--color' : ''}`}
                         >
-                            <Link href="/" className='relative w-[166px] h-[115px] p-5 pb-11 block z-10'>
-                                <h3 className="text-black text-lg">
+                            <Link href="/" className='meal-item__link relative block z-10'>
+                                <h3 className="meal-item__title text-black">
                                     {parse(item.title)}
                                 </h3>
-                                <div className="img-box md:w-auto h-20 absolute w-20 bottom-[-17px] right-0">
+                                <div className="img-box absolute w-20 bottom-[-17px] right-0">
                                     <img
                                         src={item.image}
                                         alt={item.alt}
