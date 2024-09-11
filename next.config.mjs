021c205d752d4416a768d7e76c0de721fc2ca11d
ghlejr316 <ghlejr316@naver.com>
2024-09-11 18:42:42 +0900

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/company-project'
+    basePath: '/(company-project)'
 };
 
 export default nextConfig;
