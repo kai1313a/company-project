8e1acabb9b63a0ae0f437719e45fa311e1d78012
ghlejr316 <ghlejr316@naver.com>
2024-09-24 18:13:09 +0900

    post 서버 요청 테스트

---
+++ src/app/controller/Controller.js
@@ -0,0 +1,16 @@
+import { constant } from "@/app/util/constant";
+
+export const controller = (value) => {
+    if (constant.MAIN === value) {
+
+    }
+
+    if (constant.MAIN === value) {
+        
+    }
+
+    if (constant.MAIN === value) {
+        return addAbortSignal;
+    }
+}
+
