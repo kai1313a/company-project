4ce6648afd3f9aae01d27f707fb17c251eb4e992
saseunghan <han7182002@naver.com>
2024-09-25 12:23:22 +0900

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const repo = 'https://kai1313a.github.io/company-project/' // GitHub 저장소 이름을 여기에 입력하세요
+
 const nextConfig = {
     output: 'export',
+    basePath: `/${repo}`,
+    assetPrefix: `/${repo}/`,
     images: {
         unoptimized: true
     },
