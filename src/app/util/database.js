4b5611e34bb563dae27122820352538293a839e5
ghlejr316 <ghlejr316@naver.com>
2024-10-16 14:10:23 +0900

    make 에러 수정, database option 해제

--- src/app/util/database.js
+++ src/app/util/database.js
@@ -1,16 +1,15 @@
 import { MongoClient } from 'mongodb'
 const url = 'mongodb+srv://admin:dnstjq13@unseop.be9440o.mongodb.net/'
-const options = { useNewUrlParser: true }
 let connectDB
 
 // 개발중일때 저장하거나 새로고침만 해도 해당 DB를 계속 불러오는데 과데이터 보호를 위해 전에 저장했던 데이터가 변경사항이 없으면 그대로 사용
 if (process.env.NODE_ENV === 'development') {
   if (!global._mongo) {
-    global._mongo = new MongoClient(url, options).connect()
+    global._mongo = new MongoClient(url).connect()
   }
   connectDB = global._mongo
 } else {
-  connectDB = new MongoClient(url, options).connect()
+  connectDB = new MongoClient(url).connect()
 }
 
 export { connectDB }
